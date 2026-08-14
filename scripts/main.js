b146e663a15d184e7017289ebb61a72860d883aa
Caleb Bucci <booch2nd@gmail.com>
2026-08-13 22:21:12 -0400

    first draft of website complete

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+function initClipboardButton() {
+
+    //Grabs email address button and returns early if it doesn't exist
+    var btn = document.getElementById("copy-email-button");
+    if (!btn) {
+        return;
+    }
+
+    var email = btn.textContent.trim();
+    var timer;
+
+    function flash() {
+        btn.classList.add("copied");
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+            btn.classList.remove("copied");
+        }, 2000);
+    }
+
+    //Adds copied class to btn for 2 seconds, and copies button text to clipboard
+    btn.addEventListener('click', function () {
+        if (!navigator.clipboard) {
+            navigator.clipboard.writeText(email).then(flash);
+        } else {
+            flash();
+        }
+    });
+
+}
+
+initClipboardButton();
