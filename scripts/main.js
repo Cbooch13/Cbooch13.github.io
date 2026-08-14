b678d15a4bb95ee5a492be2fb3963495b7e6281b
Caleb Bucci <booch2nd@gmail.com>
2026-08-14 12:53:20 -0400

    fixed copy to clipboard

--- scripts/main.js
+++ scripts/main.js
@@ -9,17 +9,18 @@ function initClipboardButton() {
     var email = btn.textContent.trim();
     var timer;
 
+    //Switches icon to check and stays active for 2 seconds
     function flash() {
         btn.classList.add("copied");
         clearTimeout(timer);
-        timer = setTimeout(function() {
+        timer = setTimeout(function () {
             btn.classList.remove("copied");
         }, 2000);
     }
 
     //Adds copied class to btn for 2 seconds, and copies button text to clipboard
     btn.addEventListener('click', function () {
-        if (!navigator.clipboard) {
+        if (navigator.clipboard) {
             navigator.clipboard.writeText(email).then(flash);
         } else {
             flash();
